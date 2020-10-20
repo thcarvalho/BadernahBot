2272c8966319b1121196b1d4081eddbae7503a96
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-10-20 19:21:33 -0300

    Bot adicionado

---
+++ index.js
@@ -0,0 +1,92 @@
+const Discord = require('discord.js')
+const config = require('./config.json')
+const ms = require('ms')
+require('dotenv/config')
+
+const bot = new Discord.Client()
+bot.login(`${process.env.DISCORD_TOKEN}`)
+
+bot.once('ready', () => {
+  console.log("Bot Ready");
+})
+
+bot.on("guildMemberAdd", member => {
+  member.send(`Seja bem vindo(a) ${member.user.tag}`)
+})
+
+bot.on("message", async message => {
+  if (message.author.bot) return
+  if (message.channel.type === "dm") return
+
+  if (!message.member.hasPermission("ADMINISTRATOR")) {
+    message.reply("Hoje não")
+    return;
+  }
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g)
+  const command = args.shift().toLowerCase()
+
+  if (command === "kick") {
+    const user = message.mentions.users.first();
+    const member = message.guild.member(user)
+
+    member.kick().then(() => {
+      message.reply(`Membro ${user.tag} kickado`)
+    })
+  }
+
+  if (command === "ban") {
+    const user = message.mentions.users.first();
+    const member = message.guild.member(user)
+
+    member.ban({
+      reason: 'Muita viadagi'
+    })
+      .then(() => {
+        message.reply(`Membro ${user.tag} banido`)
+      })
+  }
+
+  if (command === "mute") {
+    const user = message.mentions.users.first();
+    const member = message.guild.member(user)
+
+    let muterole = message.guild.roles.cache.get('734460711249379368')
+    let verifiedRole = message.guild.roles.cache.get('734474317118570596')
+
+    if (muterole) {
+      try {
+        member.roles.add(muterole)
+        member.roles.remove(verifiedRole)
+        message.channel.send(`${user.tag} mutado`)
+
+        setTimeout(() => {
+          member.roles.add(verifiedRole)
+          member.roles.remove(muterole)
+          message.channel.send(`${user.tag} desmutado`)
+        }, ms('1m'))
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
+  if (command === "unmute") {
+    const user = message.mentions.users.first();
+    const member = message.guild.member(user)
+
+    let muterole = message.guild.roles.cache.get('734460711249379368')
+    let verifiedRole = message.guild.roles.cache.get('734474317118570596')
+
+    if (muterole) {
+      try {
+        member.roles.add(verifiedRole)
+        member.roles.remove(muterole)
+        message.channel.send(`${user.tag} desmutado`)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+})
