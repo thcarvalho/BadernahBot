315176604583769389c0042aa48bbb0033b4000d
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-10-26 23:20:46 -0300

    comandos extraidos para arquivos separados

--- index.js
+++ index.js
@@ -1,114 +1,43 @@
 const Discord = require('discord.js')
 const config = require('./config.json')
-const ms = require('ms')
+const kick = require('./commands/kick')
+const ban = require('./commands/ban')
+const { mute } = require('./commands/mute')
+const { unmute } = require('./commands/unmute')
+const { EventEmitter } = require('events')
+
 require('dotenv/config')
 
 const bot = new Discord.Client()
+const emitter = new EventEmitter()
+
 bot.login(process.env.DISCORD_TOKEN)
 
 bot.once('ready', () => {
   console.log("Bot Ready");
 })
 
-let muteTimeout;
-
 bot.on("message", async message => {
   if (message.author.bot) return
   if (message.channel.type === "dm") return
-
-  if (!message.member.hasPermission("ADMINISTRATOR")) {
-    message.reply("Hoje não")
-    return;
-  }
+  if (!message.member.hasPermission("ADMINISTRATOR")) return
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g)
   const command = args.shift().toLowerCase()
 
   if (command === "kick") {
-    const user = message.mentions.users.first();
-    const member = message.guild.member(user)
-
-    member.kick().then(() => {
-      message.reply(`Membro ${user.tag} kickado`)
-    })
+    await kick(message)
   }
 
   if (command === "ban") {
-    const user = message.mentions.users.first();
-    const member = message.guild.member(user)
-
-    member.ban({
-      reason: 'Muita viadagi'
-    })
-      .then(() => {
-        message.reply(`Membro ${user.tag} banido`)
-      })
+    await ban(message)
   }
 
   if (command === "mute") {
-    const user = message.mentions.users.first();
-    const member = message.guild.member(user)
-
-    // let muterole = message.guild.roles.cache.get('734460711249379368')
-    // let verifiedRole = message.guild.roles.cache.get('734474317118570596')
-
-    let muterole
-
-    try {
-      muterole = await message.guild.roles.create({
-        data: {
-          name: "Mutado",
-          color: "DARK_RED",
-          permissions: []
-        }
-      })
-
-      message.channel.updateOverwrite(muterole, { SEND_MESSAGES: false })
-
-      message.guild.channels.cache.forEach(async channel => {
-        await channel.updateOverwrite(muterole, {
-          SEND_MESSAGES: false,
-          ADD_REACTIONS: false,
-        })
-      })
-
-      member.roles.add(muterole.id)
-      message.channel.send(`${member} mutado`)
-      message.guild.roles.cache.set('@muterole', muterole)
-
-      muteTimeout = setTimeout(() => {
-        try {
-          if (muterole) {
-            member.roles.remove(muterole.id)
-            message.channel.send(`${member} desmutado`)
-            message.guild.roles.cache.get(muterole.id).delete()
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      }, 10000)
-
-    } catch (error) {
-      console.log(error);
-    }
-
+    await mute(message, emitter)
   }
 
   if (command === "unmute") {
-    const user = message.mentions.users.first();
-    const member = message.guild.member(user)
-
-    const muterole = message.guild.roles.cache.get('@muterole');
-
-    if (muterole) {
-      try {
-        clearTimeout(muteTimeout)
-        member.roles.remove(muterole.id)
-        message.channel.send(`${member} desmutado`)
-        message.guild.roles.cache.get(muterole.id).delete()
-      } catch (error) {
-        console.log(error);
-      }
-    }
+    await unmute(message, emitter)
   }
 })
